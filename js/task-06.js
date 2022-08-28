f5144f4540b65695e8ba580ef64373ef5d7abc08
Nataliiazahorodko <n.zagorodko@gmail.com>
2022-08-28 21:47:55 +0300

    task-06

---
+++ js/task-06.js
@@ -0,0 +1,21 @@
+const refs = {
+  validationInput: document.querySelector('#validation-input'),
+};
+
+const testValidation = (event) => {
+  event.currentTarget.value.length >
+  refs.validationInput.dataset.length
+    ? refs.validationInput.classList.add('invalid')
+    : refs.validationInput.classList.add('valid');
+
+  if (event.currentTarget.value === '') {
+    refs.validationInput.classList.remove('valid', 'invalid');
+  }
+};
+
+const resetValidation = (event) => {
+  refs.validationInput.classList.remove('valid', 'invalid');
+};
+
+refs.validationInput.addEventListener('blur', testValidation);
+refs.validationInput.addEventListener('focus', resetValidation);
