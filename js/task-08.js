4351ee5eb9032eed292957fa00b3162dcc60bfa4
Nataliiazahorodko <n.zagorodko@gmail.com>
2022-08-28 23:08:24 +0300

    task-08

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,19 @@
+const form = document.querySelector('.login-form');
 
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+  const form = event.currentTarget.elements;
+  const email = form.email.value;
+  const password = form.password.value;
+  console.log(email);
+  console.log(password);
+
+  if (email === '' || password === '') {
+    return alert('Всі поля повинні бути заповнені');
+  } else {
+    console.log({ email, password });
+    form.reset();
+  }
+}
