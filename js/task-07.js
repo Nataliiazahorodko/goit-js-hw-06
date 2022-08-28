1b443ab1c1d265b4a4f540bf389137e680c22378
Nataliiazahorodko <n.zagorodko@gmail.com>
2022-08-28 22:26:15 +0300

    task-07

---
+++ js/task-07.js
@@ -0,0 +1,9 @@
+const sizeControl = document.querySelector(`#font-size-control`);
+const text = document.querySelector('#text');
+
+text.style.fontSize = `${sizeControl.value}px`;
+
+sizeControl.addEventListener(
+  'input',
+  () => (text.style.fontSize = `${sizeControl.value}px`)
+);
