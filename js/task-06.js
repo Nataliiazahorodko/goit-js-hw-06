b396d2696e98b07c543fe934b6d50a1828f82251
Nataliiazahorodko <n.zagorodko@gmail.com>
2022-09-05 00:08:35 +0300

    update task-06

--- js/task-06.js
+++ js/task-06.js
@@ -2,20 +2,10 @@ const refs = {
   validationInput: document.querySelector('#validation-input'),
 };
 
-const testValidation = (event) => {
-  event.currentTarget.value.length >
-  refs.validationInput.dataset.length
-    ? refs.validationInput.classList.add('invalid')
-    : refs.validationInput.classList.add('valid');
+refs.validationInput.addEventListener("blur", (event) => {
+  const { value } = event.currentTarget;
 
-  if (event.currentTarget.value === '') {
-    refs.validationInput.classList.remove('valid', 'invalid');
-  }
-};
-
-const resetValidation = (event) => {
-  refs.validationInput.classList.remove('valid', 'invalid');
-};
-
-refs.validationInput.addEventListener('blur', testValidation);
-refs.validationInput.addEventListener('focus', resetValidation);
+  value.length == refs.validationInput.dataset.length
+    ? (refs.validationInput.classList = "valid")
+    : (refs.validationInput.classList = "invalid");
+});
