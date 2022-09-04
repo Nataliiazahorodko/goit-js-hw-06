e0edec6e6f3f345573a2dc3410f7597e181ce7c3
Nataliiazahorodko <n.zagorodko@gmail.com>
2022-09-04 23:10:49 +0300

    update task-05

--- js/task-05.js
+++ js/task-05.js
@@ -6,7 +6,7 @@ input.addEventListener('input', newInput);
 function newInput(a) {
     span.textContent = a.currentTarget.value;
 
-    if (a.currentTarget.value === " ") {
+    if (a.currentTarget.value === "") {
         span.textContent = "Anonymous";
     }
 }
